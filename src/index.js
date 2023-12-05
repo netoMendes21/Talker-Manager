20cdf80da4a8911543c302f6114bad8095915a5b
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-12-05 19:20:28 -0300

    iniciando projeto e req 1 feito

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,13 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile(path.join(__dirname, 'talker.json'), 'utf-8');
+  const talkersJson = JSON.parse(talkers);
+  if (!talkersJson) return res.status(200).json([]);
+  return res.status(200).json(talkersJson);
 });
 
 app.listen(PORT, () => {

698d356cf7cb2f4de1821f7dd53e34c5d0f8a21b
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-12-06 14:33:27 -0300

    reqs 3 e 4 finalizados

--- src/index.js
+++ src/index.js
@@ -29,6 +29,37 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(200).json(talker);
 });
 
+const geradorToken = () => {
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  
+  for (let i = 0; i < 16; i += 1) {
+    token += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return token;
+};
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' }); 
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+  }
+  if (!password) {
+    return res.status(400)
+      .json({ message: 'O campo "password" é obrigatório' }); 
+  }
+  if (password.length < 6) {
+    return res.status(400)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+  }
+  const token = geradorToken();
+  return res.status(200).json({ token });
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });

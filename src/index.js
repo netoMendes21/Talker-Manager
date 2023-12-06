78df9562acbdc6c797ff13e7d0015840499600b9
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2023-12-06 13:56:42 -0300

    req 2 finalizado e corrigido

--- src/index.js
+++ src/index.js
@@ -24,10 +24,12 @@ app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
   const talkerId = await fs.readFile(path.join(__dirname, 'talker.json'), 'utf-8');
   const talkerIdJson = JSON.parse(talkerId);
-  const talker = talkerIdJson.find((talker) => talker.id === Number(id));
-  if(!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  const talker = talkerIdJson.find((talkerFind) => talkerFind.id === Number(id));
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   return res.status(200).json(talker);
-})
+});
+
+app.get
 
 app.listen(PORT, () => {
   console.log('Online');
